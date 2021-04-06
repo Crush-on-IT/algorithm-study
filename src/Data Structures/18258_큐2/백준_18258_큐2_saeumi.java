ee614a413519a02ec4b15e37c4d583fbb745d296
saeumi <saeumi@kakao.com>
2021-04-07 00:55:14 +0900

    백준_18258_큐2

---
+++ src/Data Structures/18258_큐2/백준_18258_큐2_saeumi.java
@@ -0,0 +1,67 @@
+package Data_Structures;
+
+import java.io.*;
+import java.util.*;
+
+public class 백준_18258_큐2 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		Queue<Integer> queue = new LinkedList<Integer>();
+		StringBuilder sb = new StringBuilder();
+		int back = 0;
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			 String input = st.nextToken();
+			if (input.contains("push")) {
+				int input2 = Integer.parseInt(st.nextToken());
+				queue.offer(input2);
+				back = input2;
+			} else if (input.equals("pop")) {
+				sb.append(queue.isEmpty() ? -1 : queue.poll()).append("\n");
+			} else if (input.equals("size")) {
+				sb.append(queue.size()).append("\n");
+			} else if (input.equals("empty")) {
+				sb.append(queue.isEmpty() ? 1 : 0).append("\n");
+			} else if (input.contains("front")) {
+				sb.append(queue.isEmpty() ? -1 : queue.peek()).append("\n");
+			} else if (input.contains("back")) {
+				sb.append(queue.isEmpty() ? -1 : back).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
+
+//		Scanner sc = new Scanner(System.in);
+//		int N = sc.nextInt();
+//		
+//		Queue<Integer> queue = new LinkedList<Integer>();
+//		StringBuilder sb = new StringBuilder();
+//		int back =0;
+//		
+//		for(int i =0; i<N; i++) {
+//			String input = sc.next();
+//			if(input.contains("push")) {
+//				int input2 = sc.nextInt();
+//				queue.offer(input2);
+//				back = input2;
+//			} else if (input.equals("pop")) {
+//				sb.append(queue.isEmpty() ? -1 : queue.poll()).append("\n");
+//			} else if(input.equals("size")) {
+//				sb.append(queue.size()).append("\n");
+//			} else if (input.equals("empty")) {
+//				sb.append(queue.isEmpty() ? 1 : 0).append("\n");
+//			} else if(input.contains("front")) {
+//				sb.append(queue.isEmpty() ? -1 : queue.peek()).append("\n");
+//			} else if(input.contains("back")) {
+//				sb.append(queue.isEmpty() ? -1 : back).append("\n");
+//			}
+//		}
+//		System.out.println(sb);
+//	}
