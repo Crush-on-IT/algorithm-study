0d7e793e8aa74971cb52a2186bb8a1d8adb735db
sumin416 <sumin416@naver.com>
2021-04-10 02:59:39 +0900

    210410 solve 4 problems

---
+++ src/Data Structures/11279_최대힙/백준_11279_최대힙_sumin416.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2-o1;
+			}
+		});
+		StringBuilder sb = new StringBuilder();
+		
+		int cur;
+		for (int i = 0; i < N; i++) {
+			cur = Integer.parseInt(br.readLine());
+			
+			switch(cur) {
+				case 0:
+					sb.append(pq.isEmpty()?0:pq.poll()).append("\n");
+					break;
+				default:
+					pq.offer(cur);
+					break;
+			}
+		}
+		System.out.println(sb.toString());
+	}
+
+}
