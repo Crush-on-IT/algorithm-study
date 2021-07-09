210aa90cc580a3c7c89db7706493981c0cede688
oct14jh <khyun7621@naver.com>
2021-07-09 17:53:26 +0900

    백준_1976_여행가자_oct14jh

--- src/Disjoint Set/BOJ_1976_여행가자/백준_1976_여행가자_oct14jh.java
+++ src/Disjoint Set/BOJ_1976_여행가자/백준_1976_여행가자_oct14jh.java
@@ -1,77 +1,71 @@
+package problems;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
- 
-public class 백준_1976_여행가자_oct14jh {
-    static int[] parent;
- 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
- 
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
- 
-        parent = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            parent[i] = i;
-        }
- 
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                int temp = Integer.parseInt(st.nextToken());
- 
-                if (temp == 1) {
-                    union(i, j);
-                }
-            }
-        }
- 
-        st = new StringTokenizer(br.readLine());
-        int start = find(Integer.parseInt(st.nextToken()));
-        for (int i = 1; i < M; i++) {
-            int now = Integer.parseInt(st.nextToken());
- 
- 
-            if (start != find(now)) {
-                bw.write("NO\n");
-                bw.flush();
-                bw.close();
-                br.close();
-                return;
-            }
-        }
- 
-        bw.write("YES\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
- 
-    public static int find(int x) {
-        if (x == parent[x]) {
-            return x;
-        }
- 
-        return parent[x] = find(parent[x]);
-    }
- 
-    public static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
- 
-        if (x != y) {
-            if (x < y) {
-                parent[y] = x;
-            } else {
-                parent[x] = y;
-            }
-        }
-    }
- 
+
+public class BOJ_1976_여행가자_G4 {
+	private static int N, M;
+	private static int[] parents;
+	private static String answer = "";
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		
+		parents = new int[N];
+		for(int i = 0; i < N; i++) {
+			parents[i] = i;
+		}
+		
+		StringTokenizer st = null;
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j = 0; j < N; j++) {
+				int status = Integer.parseInt(st.nextToken());
+				if(status == 1) {
+					unionSet(i, j);
+				}
+			}
+		}
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		int[] course = new int[st.countTokens()];
+		for(int i = 0; i < course.length; i++) {
+			course[i] = Integer.parseInt(st.nextToken()) - 1;
+		}
+		
+		for(int check = 0; check < course.length - 1; check++) {
+			int placeA = course[check];
+			int placeB = course[check + 1];
+			
+			if(findSet(placeA) != findSet(placeB)) {
+				answer = "NO";
+				break;
+			} else {
+				answer = "YES";
+			}
+		}
+		
+		System.out.println(answer);
+		
+		br.close();
+	}
+
+	private static int findSet(int placeA) {
+		if(parents[placeA] == placeA)
+			return placeA;
+		return parents[placeA] = findSet(parents[placeA]);
+	}
+
+	private static void unionSet(int i, int j) {
+		int iRoot = findSet(i);
+		int jRoot = findSet(j);
+		if(iRoot != jRoot)
+			parents[jRoot] = iRoot;
+	}
 }
